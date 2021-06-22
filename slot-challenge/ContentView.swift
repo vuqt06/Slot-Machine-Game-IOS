16168834b57c3810cd62b8eb3264b4a6472b0fa5
vuqt <vuqt@umich.edu>
2021-06-22 21:48:22 +0700

    Initial Commit

---
+++ slot-challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  slot-challenge
+//
+//  Created by Vu Trinh on 6/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ slot-challenge/slot_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_challengeApp.swift
+//  slot-challenge
+//
+//  Created by Vu Trinh on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct slot_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
